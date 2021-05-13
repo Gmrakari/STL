0409a066ec104cf0b2109563ad9a718a6a321cb9
Gmrakari <gmrakari@outlook.com>
2021-05-13 21:39:59 +0800

    02_uninitialized_fill.cpp

--- 02_allocator/11_memory_tools/02_uninitialized_fill.cpp
+++ 02_allocator/11_memory_tools/02_uninitialized_fill.cpp
@@ -20,3 +20,30 @@ void uninitialized_fill(ForwardIterator first,ForwardIterator last,const T& x);
  */
 
 
+template <class ForwardIterator,class T>
+void uninitialized_fill(ForwardIterator first,ForwardIterator last,const T& x)
+{
+	__uninitialized_fill(first,last,x,value_type(first));
+}
+
+template <class ForwardIterator,class T>
+inline void __uninitialized_fill(ForwardIterator first,ForwardIterator last,const T& x,T1*)
+{
+	typedef typename __type_traits<T1>::is_POD_type is_POD;
+	__uninitialized_fill_aux(first,last,is_POD());
+}
+
+template <class ForwardIterator,class T>
+inline void __uninitialized_fill_aux(ForwardIterator first,ForwardIterator last,const T& x,__true_type)
+{
+	fill(first,last,x);
+}
+
+
+template <class ForwardIterator,class T>
+inline void __uninitialized_fill_aux(ForwardIterator first,ForwardIterator last,const T& x,__false_type)
+{
+	ForwardIterator cur = first;
+	for(; cur != last;++cur)
+		construct(&*cur,x);
+}
