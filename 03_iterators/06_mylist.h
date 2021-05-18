e00c4cbe02cbef3fa2e0b459c417da87d8afa6c6
Gmrakari <gmrakari@outlook.com>
2021-05-18 20:57:52 +0800

    06_mylist.h

--- 03_iterators/06_mylist.h
+++ 03_iterators/06_mylist.h
@@ -6,7 +6,17 @@
  *
  */
 
-#include <iostream>
+template <typename T>
+class ListItem
+{
+public:
+	T value() const { return _value; }
+	ListItem* next() const { return _next; }
+
+private:
+	T _value;
+	ListItem* _next;
+};
 
 template <typename T>
 class List
@@ -19,17 +29,6 @@ private:
 	ListItem<T>* _end;
 	ListItem<T>* _front;
 	long _size;
-};
-template <typename T>
-class ListItem
-{
-public:
-	T value() const { return _value; }
-	ListItem* next() const { return _next; }
-
-private:
-	T _value;
-	ListItem* _next;
 };
 
 //我们要设计一个iterator
