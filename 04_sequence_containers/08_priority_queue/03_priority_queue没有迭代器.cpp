2f75cc3023f4e5dda1b561470d657f3f650f06e4
Gmrakari <gmrakari@outlook.com>
2021-07-21 15:36:52 +0800

    03_priority_queue没有迭代器.cpp

---
+++ 04_sequence_containers/08_priority_queue/03_priority_queue没有迭代器.cpp
@@ -0,0 +1,15 @@
+
+
+/*
+ * Date:2021-07-21 15:35
+ * filename:03_priority_queue没有迭代器.cpp
+ *
+ */
+
+/*
+ * priority_queue的所有元素，进出都有一定的规则，只有queue顶端的元素(权值最高者)
+ * 才有机会被外界取用。priority_queue不提供遍历功能，也不提供迭代器
+ *
+ */
+
+
