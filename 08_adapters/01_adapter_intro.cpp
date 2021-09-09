2fc4a5cb432e9009b2b8c247eb4c2b0c2bddf121
Gmrakari <gmrakari@outlook.com>
2021-09-09 11:23:22 +0800

    01_adapter_intro.cpp

---
+++ 08_adapters/01_adapter_intro.cpp
@@ -0,0 +1,21 @@
+
+
+/*
+ * Date:2021-09-09 11:19
+ * filename:01_adapter_intro.cpp
+ *
+ */
+
+/*
+ * 配接器(adapters)在STL组建的灵活组合运用功能上，扮演着轴承、转换器的角色
+ * <Design Patterns>:
+ * 将一个class的接口转换到另一个class的接口，使原本因接口不兼容而不能合作的classes,可以一起运作
+ */
+
+/*
+ * 改变仿函数接口者 称为 function adapter
+ * 改变容器 称为 container adapter
+ * 改变迭代器 称为 iterator adaper
+ *
+ */
+
